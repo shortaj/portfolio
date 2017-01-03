a3cb1bc768c360ebfb8a544a49e3878f524a33fc
Alex <ajshort2010@hotmail.com>
2017-01-02 22:03:18 -0800

    made constructor function, added stuff. yep.

--- Controller/scripts.js
+++ Controller/scripts.js
@@ -1,1 +1,10 @@
 'use strict';
+
+//So they are looking for a constructor function to grab valuable data from previous/future projects to add to my portfolio. As far as I know, time spent, collaborators, and what was learned would be revelant information people would want to see... also the means of finding said project(url?)
+
+function Project(name, timeSpent, collaborators, address) {
+  this.name= name;
+  this.timeSpent= timeSpent;
+  this.collaborators= collaborators;
+  this.address= address;
+}
